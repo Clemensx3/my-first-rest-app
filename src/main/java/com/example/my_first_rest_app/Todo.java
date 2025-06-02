d75900ee36272f38b2cf0ea390fe6a0970255994
Clemens Brunner <brunnerclemens58@gmail.com>
2025-06-02 20:58:56 +0200

    Added Get and Post Mapping with JSON body

---
+++ src/main/java/com/example/my_first_rest_app/Todo.java
@@ -0,0 +1,30 @@
+package com.example.my_first_rest_app;
+
+public class Todo {
+
+    private Integer id;
+    private String description;
+    private boolean isDone;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId(){
+        return this.id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setIsDone(Boolean isDone) {
+        this.isDone = isDone;
+    }
+    public Boolean getIsDone() {
+        return this.isDone;
+    }
+
+}
--- src/main/java/com/example/my_first_rest_app/TodoController.java
+++ src/main/java/com/example/my_first_rest_app/TodoController.java
@@ -2,20 +2,25 @@ package com.example.my_first_rest_app;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TodoController {
 
-    @GetMapping("/greet")
-    public ResponseEntity<String> hello(@RequestParam(value = "name") String name) {
+    @GetMapping("/todo")
+    public ResponseEntity<Todo> get(@RequestParam(value = "id") int id) {
 
-        if(name.equals("admin")) {
-            return new ResponseEntity<String>("Hello " + name, HttpStatus.OK);
-        }
+        Todo newTodo = new Todo();
+        newTodo.setId(id);
+        newTodo.setDescription("Einkaufen gehen");
+        newTodo.setIsDone(true);
 
-        return new ResponseEntity<String>("Error", HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
+    }
+
+    @PostMapping("/todo")
+    public ResponseEntity<Todo> create(@RequestBody Todo newTodo) {
+        // save todo in db
+        return new ResponseEntity<Todo>(newTodo, HttpStatus.OK);
     }
 }
